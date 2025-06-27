bfa47be9751dfcfdea4d3f13049f5d5553c9f7cc
Abdul Haseeb <haseeb.a@2basetechnologies.com>
2025-06-27 21:26:50 +0530

    Add JSON parsing and weather information extraction functionality.

--- tools/weather/weather_forecast_report_agent_tool.py
+++ tools/weather/weather_forecast_report_agent_tool.py
@@ -1,3 +1,5 @@
+import json
+
 from ibm_watsonx_orchestrate.agent_builder.tools import tool, ToolPermission
 
 
@@ -13,4 +15,27 @@ def generate_weather_forecast_report_tool(weather_forcast_json: str):
     :param weather_forcast_json: Input json containing weather report of the day
     :return: String containing human-readable report of weather
     '''
+
+    json_data = json.loads(weather_forcast_json)
+
+    result = _extract_weather_info(json_data["list"])
+
     pass
+
+
+def _extract_weather_info(list_data):
+    result = []
+
+    for item in list_data:
+        try:
+            date = item["dt_txt"]
+            weather_object = item["weather"][0]
+            weather = f'{weather_object["main"]}, {weather_object["description"]}'
+
+            text = f"{date} - {weather}"
+
+            result.append(text)
+        except Exception as e:
+            print(e)
+
+    return result
